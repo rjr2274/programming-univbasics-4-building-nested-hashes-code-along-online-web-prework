7b13344dfdbab295a5041e4b4a3452efbd486b91
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-11 03:47:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,7 +7,7 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {
-        :name => "Bonvolio",
+        :name => "Benvolio",
         :age => "17",
         :attitude => "worried"
       },
